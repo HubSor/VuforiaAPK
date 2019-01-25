ef087a57978f920ccc8474114f0e0c3cce0a0035
HubSor <44928255+HubSor@users.noreply.github.com>
2019-01-25 20:45:01 +0100

    Niewielkie zmiany

---
+++ Assets/UI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour {
+
+    public GameObject ui;
+
+    public Camera c;
+
+    private void FixedUpdate()
+    {
+        if (GameObject.FindGameObjectWithTag("Piotruś") != null) ui.SetActive(true);
+        else ui.SetActive(false);
+            
+
+    }
+    public void Zoomin()
+    {
+        c.fieldOfView = 35;
+    }
+
+
+}
