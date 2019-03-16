287d27cf8732ac06eb452289932cb169d506ae8e
HubSor <44928255+HubSor@users.noreply.github.com>
2019-03-16 20:04:00 +0100

    prawie wszystko zrobione

--- Assets/UI.cs
+++ Assets/UI.cs
@@ -4,36 +4,23 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UI : MonoBehaviour {
-	public Touch[] Touches; 
+public class UI : MonoBehaviour
+{
     public GameObject ui;
-	public float perspectiveZoomSpeed = 0.5f;        // The rate of change of the field of view in perspective mode.
-	public float orthoZoomSpeed = 0.5f; // The rate of change of the orthographic size in orthographic mode.
     public Text kawalekuizciekawostka;
- 
+    public Touch Firsttouch;
+
     public void Wyswietlciekwersjaostateczna()
     {
         var temp = GameObject.FindGameObjectsWithTag("Przedmiot");
         if (temp.Any(x => x.GetComponent<Renderer>().enabled))
-        { 
+        {
             kawalekuizciekawostka.gameObject.SetActive(!kawalekuizciekawostka.gameObject.activeInHierarchy);
         }
     }
-    public void RotateRight()
-	{
-		GameObject.FindGameObjectWithTag("Przedmiot")?.transform?.Rotate(new Vector3(0, 45));
-	}
-
-	public void RotateLeft()
-	{
-		GameObject.FindGameObjectWithTag("Przedmiot")?.transform?.Rotate(new Vector3(0,-45));
-	}
-
-	public Camera camera;
 
     private void FixedUpdate()
     {
-		Touches=Input.touches;
         var temp = GameObject.FindGameObjectsWithTag("Przedmiot");
         if (temp.Any(x => x.GetComponent<Renderer>().enabled))
         {
@@ -41,50 +28,63 @@ public class UI : MonoBehaviour {
             var t = temp.First(x => x.GetComponent<Renderer>().enabled);
             kawalekuizciekawostka.text = t.GetComponent<text>().tekstciekawostki;
         }
-        else 
+        else
         {
             ui.SetActive(false);
             kawalekuizciekawostka.gameObject.SetActive(false);
         }
-		// If there are two touches on the device...
-		if (Input.touchCount == 2)
-		{
-			// Store both touches.
-			Touch touchZero = Input.GetTouch(0);
-			Touch touchOne = Input.GetTouch(1);
+        // If there are two touches on the device...
+#if UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+        //Check if Input has registered more than zero touches
+        if (Input.touchCount == 1)
+        {
+            //Store the first touch detected.
+            Touch myTouch = Input.touches[0];
 
-			// Find the position in the previous frame of each touch.
-			Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
-			Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+            //Check if the phase of that touch equals Began
+            if (myTouch.phase == TouchPhase.Began)
+            {
+                //If so, set touchOrigin to the position of that touch
+                Firsttouch = myTouch;
+            }
 
-			// Find the magnitude of the vector (the distance) between the touches in each frame.
-			float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
-			float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+            //If the touch phase is not Began :
+            else if (myTouch.phase == TouchPhase.Moved && myTouch.phase != TouchPhase.Stationary)
+            {
+                //Set touchEnd to equal the position of this touch
+                Vector2 delta = Firsttouch.position - myTouch.position;
 
-			// Find the difference in the distances between each frame.
-			float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+                delta.Normalize();
+                delta = delta * 2;
+                //delta = camera.InverseTransformDirection (delta);
+                temp.FirstOrDefault(x => x.GetComponent<Renderer>().enabled).transform.Rotate(-delta * 6);
+                Firsttouch = myTouch;
+            }
+        }
+        if (Input.touchCount >= 2)
+        {
 
-			// If the camera is orthographic...
-			if (camera.orthographic)
-			{
-				// ... change the orthographic size based on the change in distance between the touches.
-				camera.orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
-
-				// Make sure the orthographic size never drops below zero.
-				camera.orthographicSize = Mathf.Max(camera.orthographicSize, 0.1f);
-			}
-			else
-			{
-				// Otherwise change the field of view based on the change in distance between the touches.
-				camera.fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
-
-				// Clamp the field of view to make sure it's between 0 and 180.
-				camera.fieldOfView = Mathf.Clamp(camera.fieldOfView, 0.1f, 179.9f);
-			}
-		}
-
-	}
-	
+            // Store both touches.
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
 
 
+            // Find the position in the previous frame of each touch.
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            // Find the magnitude of the vector (the distance) between the touches in each frame.
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            // Find the difference in the distances between each frame.
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+            deltaMagnitudeDiff = deltaMagnitudeDiff / 125;
+
+            temp.FirstOrDefault(x => x.GetComponent<Renderer>().enabled).transform.localScale += new Vector3(1, 1, 1)* deltaMagnitudeDiff;
+
+
+        }
+#endif
+    }
 }
