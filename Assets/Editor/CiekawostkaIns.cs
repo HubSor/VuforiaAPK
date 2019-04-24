867810c35588ffd7f13a2dfd8de2c8f1b00e1a75
HubSor <44928255+HubSor@users.noreply.github.com>
2019-04-24 21:12:50 +0200

    Przerobiony tekst

---
+++ Assets/Editor/CiekawostkaIns.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+[CustomEditor (typeof(text))]
+public class CiekawostkaIns:Editor
+{
+    public override void OnInspectorGUI()
+    {
+        text temp = (text)target;
+        temp.tekstciekawostki = EditorGUILayout.TextArea("sample text: ", temp.tekstciekawostki);
+            
+    }
+}
